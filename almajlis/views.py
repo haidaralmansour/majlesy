a9cc8bc5eab3250a3593d54f8eb9cf4ee1d0e1f7
Haidar AlMansour <haidar.almansour@zain.com>
2020-02-05 10:11:52 +0300

    Done with session list and article list

--- almajlis/views.py
+++ almajlis/views.py
@@ -3,18 +3,19 @@ from .models import Citizen
 from .forms import CitizenForm
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import redirect
+from .models import Citizen, Candidate, Data_Manager, Data_Creator, Suggestion, Comment, Article, Session
 # Create your views here.
 
 
 def session_list(request):
     context = {
-        "session":Session.objects.all()
+        "sessions":Session.objects.all()
     }
     return render(request, 'session_list.html', context)
 
 def article_list(request):
     context = {
-        "article":Article.objects.all()
+        "articles":Article.objects.all()
     }
     return render(request, 'article_list.html', context)
 
